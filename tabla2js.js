6c24b3aa33109163b293bf78055a5c391b79d725
lauramarsantana <129211680+davidgonzalezzc@users.noreply.github.com>
2024-02-19 23:51:38 -0500

    add table 3 and js functions TODO:data in tb3

--- tabla2js.js
+++ tabla2js.js
@@ -1,3 +1,4 @@
+
 function validar(campoId){
     let campo = document.getElementById(campoId);
     if(campo.value==""){
@@ -37,7 +38,29 @@ function validarForm(){
     if(!validar('lastname4')){
         return;
     }
+   
+    var name1Value = document.getElementById('name1').value;
+    var lastname1Value = document.getElementById('lastname1').value;
+    var name2Value = document.getElementById('name2').value;
+    var lastname2Value = document.getElementById('lastname2').value;
+    var name3Value = document.getElementById('name3').value;
+    var lastname3Value = document.getElementById('lastname3').value;
+    var name4Value = document.getElementById('name4').value;
+    var lastname4Value = document.getElementById('lastname4').value;
+    // Construir la URL con los parámetros
+    var url = "tabla2.html?" +
+              "name1=" + encodeURIComponent(firstName1Value) + "&lastname1=" + encodeURIComponent(lastName1Value) +
+              "&name2=" + encodeURIComponent(firstName2Value) + "&lastname2=" + encodeURIComponent(lastName2Value) +
+              "&name3=" + encodeURIComponent(firstName3Value) + "&lastname3=" + encodeURIComponent(lastName3Value) +
+              "&name4=" + encodeURIComponent(firstName4Value) + "&lastname4=" + encodeURIComponent(lastName4Value);
 
-    // Solo si la validación es exitosa, cambiar la ubicación
-    window.location.href = "tabla2.html";
+    // Redirigir a la nueva página con los parámetros en la URL
+    window.location.href = url;
 }
+function volver(){
+    window.location.href= "tabla.html";
+}
+
+
+
+
--- tabla3js.js
+++ tabla3js.js
@@ -1,3 +1,26 @@
 function back(){
     window.location.href = "tabla2.html";
 }
+function leerGET(){
+    var cadGET = location.search.substr(1,location.search.length);
+    var arrGET = cadGET.split("&");
+    var asocGET = new Array();
+    var variable = "";
+    var valor = "";
+    for(i=0;i<arrGET.length;i++){
+        var aux = arrGET[i].split("=");
+        variable = aux[0];
+        valor = aux[1];
+        asocGET[variable]=valor;
+    }
+    return asocGET;
+}
+function escribir(){
+    var paresVarValor = leerGET();
+    document.getElementById("name1").innerHTML = paresVarValor["name1"];
+    document.getElementById("lastname1").innerHTML = paresVarValor["lastName1"];
+}
+
+
+
+
